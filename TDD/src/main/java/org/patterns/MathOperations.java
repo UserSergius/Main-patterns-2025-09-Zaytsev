d05fc6588826f3f8c926b98c7a261904fbc672e1
Zaytsev Sergey <sergey.zaytsev@tccenter.ru>
2025-11-12 22:15:06 +0300

    TDD create new class MathOperations.

---
+++ TDD/src/main/java/org/patterns/MathOperations.java
@@ -0,0 +1,11 @@
+package org.patterns;
+
+public class MathOperations {
+
+    private double a;
+    private double b;
+    private double c;
+
+    public void solve(double a, double b, double c) {
+    }
+}
---
+++ TDD/src/test/java/org/patterns/MathOperationsTest.java
@@ -0,0 +1,12 @@
+package org.patterns;
+
+
+import org.junit.jupiter.api.Test;
+
+class MathOperationsTest {
+
+    @Test
+    void check() {
+
+    }
+}
