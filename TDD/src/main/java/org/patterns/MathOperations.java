dfe0bebc1c6cf2b5d1baca751cd3cb26587112bc
Zaytsev Sergey <sergey.zaytsev@tccenter.ru>
2025-11-24 21:38:16 +0300

    TDD Complete homework.

--- TDD/src/main/java/org/patterns/MathOperations.java
+++ TDD/src/main/java/org/patterns/MathOperations.java
@@ -2,10 +2,33 @@ package org.patterns;
 
 public class MathOperations {
 
-    private double a;
-    private double b;
-    private double c;
+    public double[] getSquareRoots(double a, double b, double c) {
 
-    public void solve(double a, double b, double c) {
+        double x1, x2;
+        double discriminant = getDiscriminant(a, b, c);
+
+        if(discriminant > 0) {
+            x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+
+            return new double[] {x1, x2};
+        } else if(isZero(discriminant)) {
+
+            x1 = -b / (2 * a);
+
+            return new double[] {x1};
+        }
+        return new double[0];
+    }
+
+    private boolean isZero(double y) {
+        return Math.abs(y) < 1e-5;
+    }
+
+    private double getDiscriminant(double a, double b, double c) {
+        if(isZero(a)) {
+            throw new IllegalArgumentException("Аргумент \"a\" не может быть нулем!");
+        }
+        return b*b - 4*a*c;
     }
 }
